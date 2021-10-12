b48063219f5b81d02af7b0f2149b1ef751c2a7fa
kasluk345 <kaszuba.lukasz@gmail.com>
2021-10-12 17:37:28 +0200

    set up remote

---
+++ src/main/java/com/kasluk345/foreigncurrencyaccount/ForeignCurrencyAccountApplication.java
@@ -0,0 +1,13 @@
+package com.kasluk345.foreigncurrencyaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignCurrencyAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignCurrencyAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kasluk345/foreigncurrencyaccount/ForeignCurrencyAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasluk345.foreigncurrencyaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignCurrencyAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
